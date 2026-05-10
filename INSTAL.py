82601abfa00866f40f00b27ec103e0373525e741
BEXRUZ7776 <xamrozbabayev777@gmail.com>
2026-05-10 16:27:18 +0500

    Update INSTAL.py

--- INSTAL.py
+++ INSTAL.py
@@ -3,33 +3,28 @@ import threading
 from flask import Flask
 from aiogram import Bot, Dispatcher, executor, types
 
-# Bot tokeningiz
+# Bot tokeningizni tekshiring
 API_TOKEN = '8194455415:AAGZzZ_Yt3L09iP0068V_vHclY-2B8E6F0o'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
-# Render o'chib qolmasligi uchun Flask server
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is active!"
+    return "Bot is online!"
 
 def run_flask():
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
-# Bot buyruqlari
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Assalomu alaykum! Bot Render-da muvaffaqiyatli ishga tushdi! ✅")
+    await message.reply("Vaziyat nazorat ostida! Bot Render-da ishladi. ✅")
 
 if __name__ == '__main__':
-    # Flask serverni alohida oqimda ishga tushiramiz
-    t = threading.Thread(target=run_flask)
-    t.start()
-    
+    # Flaskni alohida oqimda yuritamiz
+    threading.Thread(target=run_flask).start()
     # Botni ishga tushiramiz
     executor.start_polling(dp, skip_updates=True)
     
